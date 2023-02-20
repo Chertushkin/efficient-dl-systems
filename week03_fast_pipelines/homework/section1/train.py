8d4b9211cd5bfd542a8364a8f9462bbbbda100f7
Chertushkin <chertushkinestate@gmail.com>
2023-02-21 01:04:35 +0500

    Week3 HW 3-1 Finished

--- week03_fast_pipelines/homework/section1/train.py
+++ week03_fast_pipelines/homework/section1/train.py
@@ -1,6 +1,9 @@
 import torch
 from torch import nn
 from tqdm.auto import tqdm
+import typing as tp
+from timeit import default_timer
+import numpy as np
 
 from unet import Unet
 
@@ -10,21 +13,49 @@ from dataset import get_train_data
 def train_epoch(
     train_loader: torch.utils.data.DataLoader,
     model: torch.nn.Module,
-    criterion: torch.nn._Loss,
-    optimizer: torch.optim.optimizer.Optimizer,
+    criterion: torch.nn.L1Loss,
+    optimizer: tp.Type[torch.optim.Optimizer],
     device: torch.device,
 ) -> None:
     model.train()
-
+    # print(model)
+    # factor = 1
     pbar = tqdm(enumerate(train_loader), total=len(train_loader))
     for i, (images, labels) in pbar:
         images = images.to(device)
         labels = labels.to(device)
 
+        scaler = torch.cuda.amp.GradScaler()
         with torch.cuda.amp.autocast():
             outputs = model(images)
             loss = criterion(outputs, labels)
-        # TODO: your code for loss scaling here
+            # loss *= factor
+
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+
+
+            # loss.backward()
+
+
+            # ls = []
+            # for p in model.parameters():
+            #     ls.append(torch.norm(p.grad, 2).item())
+            # grad_mean = torch.as_tensor(np.mean(ls))
+            # if grad_mean < 1:
+            #     factor = 1. / grad_mean
+            # else:
+            #     factor = 1
+
+            # loss = loss * factor
+            # loss.backward()
+            # for p in model.parameters():
+            #     p.grad = p.grad / factor
+
+            # optimizer.step()
+
+            optimizer.zero_grad()
 
         accuracy = ((outputs > 0.5) == labels).float().mean()
 
@@ -39,6 +70,14 @@ def train():
 
     train_loader = get_train_data()
 
-    num_epochs = 5
-    for epoch in range(0, num_epochs):
+    num_epochs = 3
+    start = default_timer()
+    for _ in range(0, num_epochs):
         train_epoch(train_loader, model, criterion, optimizer, device=device)
+    end = default_timer()
+
+    print(f"Took {end - start} seconds")
+
+
+if __name__ == "__main__":
+    train()
